adafa83cd6ef6369a82cf4204004c23ad68b084d
LucasVicenti12 <lucasvicenti60@gmail.com>
2025-09-21 11:56:21 -0300

    chore: force push

--- src/config/routes/default/DefaultPage.tsx
+++ src/config/routes/default/DefaultPage.tsx
@@ -2,7 +2,7 @@ import {Outlet} from "react-router-dom";
 import Layout from "../layout/Layout.tsx";
 
 export const DefaultPage = () => {
-    console.log("running 1.2")
+    console.log("running 1.3")
 
     return (
         <Layout.Root>
