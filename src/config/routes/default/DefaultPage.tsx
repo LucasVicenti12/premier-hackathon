fee129b423858887a5857247ab1d169341ef5670
LucasVicenti12 <lucasvicenti60@gmail.com>
2025-09-21 12:33:45 -0300

    chore: bump version

--- src/config/routes/default/DefaultPage.tsx
+++ src/config/routes/default/DefaultPage.tsx
@@ -2,7 +2,7 @@ import {Outlet} from "react-router-dom";
 import Layout from "../layout/Layout.tsx";
 
 export const DefaultPage = () => {
-    console.log("running 1.4")
+    console.log("running 1.5")
 
     return (
         <Layout.Root>
