6c1535ed6fe9597497f2f06259a59217261ffa1f
LucasVicenti12 <lucasvicenti60@gmail.com>
2025-09-21 09:46:02 -0300

    chore: created spanish dictionary

--- src/config/i18n/components/ToogleLanguageButton.tsx
+++ src/config/i18n/components/ToogleLanguageButton.tsx
@@ -1,29 +1,28 @@
-import {IconButton} from "@mui/joy";
+import {Option, Select} from "@mui/joy";
 import {useTranslation} from "react-i18next";
 
 import GTranslateRounded from '@mui/icons-material/GTranslateRounded';
 
 export const ToggleLanguageButton = () => {
-    const {i18n} = useTranslation()
+    const {t, i18n} = useTranslation()
 
     return (
-        <IconButton
-            sx={{
-                ":hover": {
-                    "& .MuiSvgIcon-root": {
-                        color: "#033e8c"
-                    }
-                }
-            }}
-            onClick={() => {
-                if (i18n.language === "pt") {
-                    i18n.changeLanguage("en")
-                } else {
-                    i18n.changeLanguage("pt")
-                }
+        <Select
+            startDecorator={<GTranslateRounded/>}
+            value={i18n.language}
+            onChange={async (_, value) => {
+                await i18n.changeLanguage(value as string)
             }}
         >
-            <GTranslateRounded sx={{color: "#ffffff"}}/>
-        </IconButton>
-    );
+            <Option value={"pt"}>
+                {t("portuguese")}
+            </Option>
+            <Option value={"en"}>
+                {t("english")}
+            </Option>
+            <Option value={"span"}>
+                {t("spanish")}
+            </Option>
+        </Select>
+    )
 }
